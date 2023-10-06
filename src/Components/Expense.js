5a77291e25cf63b2d9dd5f488e53262391b559b8
S-123-satya <satyaprakash5056742@gmail.com>
2023-10-06 12:39:10 +0530

    localtionOfExpenditure is added

--- src/Components/Expense.js
+++ src/Components/Expense.js
@@ -1,11 +1,15 @@
 import ExpenseDetails from "./ExpenseDetails";
 import './../index.css'
 const Expense=()=>{
+    let date='12 jan 2023';
+    let expenseItem='Movie tickets';
+    let expenseAmount=100;
+    let locationOfExpenditure="Delhi";
     return (
         <div className="p-4 rounded-lg m-40 bg-slate-600">
-            <ExpenseDetails/>
-            <ExpenseDetails/>
-            <ExpenseDetails/>
+            <ExpenseDetails data={date} expenseAmount={expenseAmount} expenseItem={expenseItem} locationOfExpenditure={locationOfExpenditure}/>
+            {/* <ExpenseDetails/>
+            <ExpenseDetails/> */}
         </div>
     )
 }
--- src/Components/ExpenseDetails.js
+++ src/Components/ExpenseDetails.js
@@ -1,9 +1,11 @@
-const ExpenseDetails =()=>{
+const ExpenseDetails =(props)=>{
+    console.log(props);
     return (
-        <div className="m-2 p-3 bg-slate-800 text-white rounded-lg">
-            <span> 12 jan 2023 </span>
-            <span> Movie tickets </span>
-            <span> $100 </span>
+        <div className="m-2 p-3 bg-slate-800 text-white rounded-lg ">
+            <span> {props.date} </span>
+            <span> {props.expenseItem} </span>
+            <span> {props.locationOfExpenditure} </span>
+            <span> {props.expenseAmount} </span>
         </div>
     )
 }
