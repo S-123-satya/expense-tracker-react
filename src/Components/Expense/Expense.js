1fb94e1cd3c64776775894ad1a00875a55a94bd8
S-123-satya <satyaprakash5056742@gmail.com>
2023-10-16 14:19:10 +0530

    filter by year function working

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -5,13 +5,19 @@ import ExpenseItem from "./ExpenseItems";
 const Expense = ({ expenses }) => {
   console.log(expenses);
   const [filteredYear,setFilteredYear]= useState('2023');
-  const filterChangeHandler= setectedYear=>{
-    setFilteredYear(setectedYear);
+  const [filteredExpense,setFilteredExpense]=useState(expenses);
+  const filterChangeHandler= selectedYear=>{
+    setFilteredYear(selectedYear);
+    const filterExpense=expenses.filter(expense=>{
+      if(expense.date.getFullYear()==selectedYear)
+        return expense;
+    })
+    setFilteredExpense(filterExpense)
   }
   return (
     <Card className="p-4 rounded-lg mx-20 m-2 bg-slate-600">
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {expenses.map((expense, i) => (
+      {filteredExpense.map((expense, i) => (
         <ExpenseItem key={i} expense={expense} />
       ))}
     </Card>
