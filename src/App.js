283b4e314aee16bccd5480e0e58122069badacd1
S-123-satya <satyaprakash5056742@gmail.com>
2023-10-16 16:08:05 +0530

    toggle functionality working

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import { useState } from "react";
 import Expense from "./Components/Expense/Expense";
 import ExpenseForm from "./Components/ExpenseForm/ExpenseForm";
 import expense from "./ExpenseModel/expenseModel";
+import Card from "./Components/UI/Card";
 
 function App() {
-  const [expenseData,setExpenseData] = useState(expense);
-  function addExpenseHandler(value){
-    setExpenseData((prevValues=>{
-      return [value, ...prevValues]
-    }));
+  const [expenseData, setExpenseData] = useState(expense);
+
+  function addExpenseHandler(value) {
+    setExpenseData((prevValues) => {
+      return [value, ...prevValues];
+    });
     console.log(expenseData);
   }
   return (
-    <div  className="bg-green-200">
+    <div className="bg-green-200">
       <ExpenseForm onAddExpense={addExpenseHandler} />
-      <Expense expenses={expenseData}/>
+      <Expense expenses={expenseData} />
     </div>
   );
 }
--- src/Components/ExpenseForm/ExpenseForm.js
+++ src/Components/ExpenseForm/ExpenseForm.js
@@ -5,60 +5,70 @@ const ExpenseForm = (props) => {
   const [expenseAmt, setExpenesAmt] = useState("");
   const [expenseTitle, setExpenesTitle] = useState("");
   const [expenseDate, setExpenesDate] = useState("");
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const toggleFormHandler = (e) => {
+    setIsFormVisible(prevValue=>!prevValue);
+  };
   function submitHandler(e) {
     e.preventDefault();
-    const expenseData={
-      title:expenseTitle,
-      amount:expenseAmt,
-      date:new Date(expenseDate),
-    }
+    const expenseData = {
+      title: expenseTitle,
+      amount: expenseAmt,
+      date: new Date(expenseDate),
+    };
     props.onAddExpense(expenseData);
     setExpenesAmt("");
     setExpenesDate("");
     setExpenesTitle("");
+    setIsFormVisible(prevValue=>!prevValue);
   }
   return (
     <Card className=" bg-slate-700 w-3/5 text-white mx-auto mt-10 p-5">
-      <form onSubmit={submitHandler}  className="flex flex-col">
-        <Card className="m-2 p-2 bg-slate-800 border border-cyan-200">
-          <label>Expense amount</label>
-          <input
-            type="number"
-            className="text-black m-2 p-1 px-3 rounded-lg"
-            name="expenseAmt"
-            placeholder="Enter your amount"
-            value={expenseAmt}
-            onChange={(e) => setExpenesAmt(e.target.value)}
-          ></input>
+      {!isFormVisible ? (
+        <Card className="m-2 flex justify-center  p-2 bg-slate-800 border border-cyan-200">
+          <button className="" onClick={toggleFormHandler}>Add New Expense</button>
         </Card>
-        <Card className="m-2 p-2 bg-slate-800 border border-cyan-200">
-          <label>Expense Title</label>
-          <input
-            type="text"
-            className="text-black m-2 p-1 px-3 rounded-lg"
-            name="expenseTitle"
-            placeholder="Enter your item title"
-            value={expenseTitle}
-            onChange={(e) => setExpenesTitle(e.target.value)}
-          ></input>
-        </Card>
-        <Card className="m-2 p-2 bg-slate-800 border border-cyan-200">
-          <label>Expense date</label>
-          <input
-            type="date"
-            className="text-black m-2 p-1 px-3 rounded-lg"
-            name="expenseDate"
-            placeholder="Enter your date"
-            value={expenseDate}
-            onChange={(e)=>setExpenesDate(e.target.value)}
-          ></input>
-        </Card>
-        <Card className="m-2 w-56 grid place-self-center p-2 bg-slate-800 border border-cyan-200">
-          <button type="submit">
-            Save
-          </button>
-        </Card>
-      </form>
+      ) : (
+        <form onSubmit={submitHandler} className="flex flex-col">
+          <Card className="m-2 p-2 bg-slate-800 border border-cyan-200">
+            <label>Expense amount</label>
+            <input
+              type="number"
+              className="text-black m-2 p-1 px-3 rounded-lg"
+              name="expenseAmt"
+              placeholder="Enter your amount"
+              value={expenseAmt}
+              onChange={(e) => setExpenesAmt(e.target.value)}
+            ></input>
+          </Card>
+          <Card className="m-2 p-2 bg-slate-800 border border-cyan-200">
+            <label>Expense Title</label>
+            <input
+              type="text"
+              className="text-black m-2 p-1 px-3 rounded-lg"
+              name="expenseTitle"
+              placeholder="Enter your item title"
+              value={expenseTitle}
+              onChange={(e) => setExpenesTitle(e.target.value)}
+            ></input>
+          </Card>
+          <Card className="m-2 p-2 bg-slate-800 border border-cyan-200">
+            <label>Expense date</label>
+            <input
+              type="date"
+              className="text-black m-2 p-1 px-3 rounded-lg"
+              name="expenseDate"
+              placeholder="Enter your date"
+              value={expenseDate}
+              onChange={(e) => setExpenesDate(e.target.value)}
+            ></input>
+          </Card>
+          <div className="flex justify-end ">
+            <button className="p-2 mx-6 bg-slate-800 border border-cyan-200 rounded-lg" onClick={toggleFormHandler}  >Cancel</button>
+            <button className="p-2 mx-2 bg-slate-800 border border-cyan-200 rounded-lg" type="submit">Save</button>
+          </div>
+        </form>
+      )}
     </Card>
   );
 };
