6b6362da63e072b2b0117dc9de911c1b15592fa7
S-123-satya <satyaprakash5056742@gmail.com>
2023-10-16 14:55:25 +0530

    conditional rending working

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -4,22 +4,28 @@ import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItems";
 const Expense = ({ expenses }) => {
   console.log(expenses);
-  const [filteredYear,setFilteredYear]= useState('2023');
-  const [filteredExpense,setFilteredExpense]=useState(expenses);
-  const filterChangeHandler= selectedYear=>{
+  const [filteredYear, setFilteredYear] = useState("2023");
+  const [filteredExpense, setFilteredExpense] = useState(expenses);
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    const filterExpense=expenses.filter(expense=>{
-      if(expense.date.getFullYear()==selectedYear)
-        return expense;
-    })
-    setFilteredExpense(filterExpense)
-  }
+    const filterExpense = expenses.filter((expense) => {
+      if (expense.date.getFullYear() == selectedYear) return expense;
+    });
+    setFilteredExpense(filterExpense);
+  };
   return (
     <Card className="p-4 rounded-lg mx-20 m-2 bg-slate-600">
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {filteredExpense.map((expense, i) => (
-        <ExpenseItem key={i} expense={expense} />
-      ))}
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {filteredExpense.length === 0 ? (
+        <p className="text-white">No Expense Items</p>
+      ) : (
+        filteredExpense.map((expense, i) => (
+          <ExpenseItem key={i} expense={expense} />
+        ))
+      )}
     </Card>
   );
 };
