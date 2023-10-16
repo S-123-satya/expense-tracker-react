003ed7ae874081cb321fc5f97c822c5e9efd7c82
S-123-satya <satyaprakash5056742@gmail.com>
2023-10-16 12:30:54 +0530

    filter by year created

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItems";
 const Expense = ({ expenses }) => {
   console.log(expenses);
-  
+  const [filteredYear,setFilteredYear]= useState('2023');
+  const filterChangeHandler= setectedYear=>{
+    setFilteredYear(setectedYear);
+  }
   return (
     <Card className="p-4 rounded-lg mx-20 m-2 bg-slate-600">
-      {expenses.map((expense,i) => (
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {expenses.map((expense, i) => (
         <ExpenseItem key={i} expense={expense} />
       ))}
     </Card>
