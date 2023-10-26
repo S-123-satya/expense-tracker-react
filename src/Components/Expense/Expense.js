becc01b5f8073651706075b82a46176fc2ec396d
S-123-satya <satyaprakash5056742@gmail.com>
2023-10-26 13:39:17 +0530

    filter expene is not working with expense

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItems";
+import ExpenseChart from "./ExpenseChart";
 const Expense = ({ expenses }) => {
   console.log(expenses);
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -12,17 +13,20 @@ const Expense = ({ expenses }) => {
       if (expense.date.getFullYear() == selectedYear) return expense;
     });
     setFilteredExpense(filterExpense);
+    console.log(filterExpense);
   };
+  console.log(filteredExpense);
   return (
     <Card className="p-4 rounded-lg mx-20 m-2 bg-slate-600">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpenseChart expenses={expenses}/>
       {filteredExpense.length === 0 ? (
         <p className="text-white">No Expense Items</p>
       ) : (
-        filteredExpense.map((expense, i) => (
+        expenses.map((expense, i) => (
           <ExpenseItem key={i} expense={expense} />
         ))
       )}
